17c8bd9c0b84dc26e2544c4dc32b2e131545fc88
Nelson Fonkoua <tambapps@gmail.com>
2025-04-22 00:01:56 +0200

    added menu

--- src/main/kotlin/com/tambapps/marcel/gymnase/GymnaseController.kt
+++ src/main/kotlin/com/tambapps/marcel/gymnase/GymnaseController.kt
@@ -1,9 +1,10 @@
 package com.tambapps.marcel.gymnase
 
-import com.tambapps.marcel.gymnase.node.MarcelCodeArea
 import com.tambapps.marcel.gymnase.service.MarcelCodeAreaFactory
+import javafx.application.Platform
 import javafx.collections.ListChangeListener
 import javafx.fxml.FXML
+import javafx.scene.control.MenuBar
 import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
 
@@ -11,7 +12,8 @@ import javafx.scene.control.TabPane
 class GymnaseController: ListChangeListener<Tab> {
   @FXML
   private lateinit var tabPane: TabPane
-
+  @FXML
+  private lateinit var menuBar: MenuBar
 
   @FXML
   fun initialize() {
@@ -19,6 +21,11 @@ class GymnaseController: ListChangeListener<Tab> {
     tabPane.tabs.addListener(this)
     val tab = tabPane.tabs.first()
     tab.content = MarcelCodeAreaFactory.create()
+  }
+
+  @FXML
+  fun close() {
+    Platform.exit()
   }
 
   private fun updateTabHeaderVisibility() {
